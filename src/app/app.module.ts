8cd5dd588a58c703e49d95ab718c30b3e49e5bee
Alex Colombo <uricaine@hotmail.com>
2019-06-22 10:03:40 +0200

    cleaning and organize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
-import { APP_ROUTES } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// Rutas
+import { APP_ROUTES } from './app.routes';
+// Modulos
+import { PageModule } from './pages/pages.module';
+// Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { PageModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, RegisterComponent],
