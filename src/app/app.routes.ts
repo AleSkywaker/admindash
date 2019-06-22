4a6ed0b6b64fc0529b8dee381266af7307e6c82e
Alex Colombo <uricaine@hotmail.com>
2019-06-22 09:53:52 +0200

    cleaning app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { RegisterComponent } from './login/register.component';
 
 const appRoutes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
-  //  {path: 'register', component: RegisterComponent},
-  { path: 'progress', component: ProgressComponent },
-  { path: 'graficas1', component: Graficas1Component },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: NopagefoundComponent }
 ];
 
