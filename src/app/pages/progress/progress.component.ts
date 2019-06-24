89824d36f4b55fd920fbd555ae0c417101aa0440
Alex Colombo <uricaine@hotmail.com>
2019-06-24 21:51:06 +0200

    add incrementar method

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ProgressComponent implements OnInit {
+  public progreso: number = 20;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  cambiarValor(valor: number) {
+    if (this.progreso >= 100 && valor > 0) {
+      this.progreso = 100;
+      return;
+    }
+    if (this.progreso <= 0 && valor < 0) {
+      this.progreso = 0;
+      return;
+    }
+    this.progreso += valor;
   }
-
 }
