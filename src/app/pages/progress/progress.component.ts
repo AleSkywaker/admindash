f9e942c3fc9bf5288b1222c23f5b93e4aa8c477b
Alex Colombo <uricaine@hotmail.com>
2019-06-25 19:22:42 +0200

    refactor the progress bar

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -11,16 +11,4 @@ export class ProgressComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-
-  cambiarValor(valor: number) {
-    if (this.progreso >= 100 && valor > 0) {
-      this.progreso = 100;
-      return;
-    }
-    if (this.progreso <= 0 && valor < 0) {
-      this.progreso = 0;
-      return;
-    }
-    this.progreso += valor;
-  }
 }
