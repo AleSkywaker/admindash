ed38e8dfb9c21120e49dc826fba1ff9276b549eb
Alex Colombo <uricaine@hotmail.com>
2019-06-27 22:05:44 +0200

    focus on input

--- src/app/component/incrementador/incrementador.component.ts
+++ src/app/component/incrementador/incrementador.component.ts
@@ -49,5 +49,6 @@ export class IncrementadorComponent implements OnInit {
     }
     this.progreso += valor;
     this.cambioValor.emit(this.progreso);
+    this.txtProgress.nativeElement.focus();
   }
 }
