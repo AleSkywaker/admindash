77c116e6d088666417f4708cc75dcc506e32bc60
Alex Colombo <uricaine@hotmail.com>
2019-06-27 21:57:03 +0200

    using output

--- src/app/component/incrementador/incrementador.component.ts
+++ src/app/component/incrementador/incrementador.component.ts
@@ -12,8 +12,20 @@ export class IncrementadorComponent implements OnInit {
   @Output() cambioValor: EventEmitter<number> = new EventEmitter();
   constructor() {}
 
-  ngOnInit() {
-    console.log('Leyenda', this.leyenda);
+  ngOnInit() {}
+  onChange(nuevoValor: number) {
+    const elementHTML: any = document.getElementsByName('progreso')[0];
+    console.log(elementHTML.value);
+
+    if (nuevoValor >= 100) {
+      this.progreso = 100;
+    } else if (nuevoValor <= 0) {
+      this.progreso = 0;
+    } else {
+      this.progreso = nuevoValor;
+    }
+    elementHTML.value = this.progreso;
+    this.cambioValor.emit(this.progreso);
   }
   cambiarValor(valor: number) {
     if (this.progreso >= 100 && valor > 0) {
@@ -25,7 +37,6 @@ export class IncrementadorComponent implements OnInit {
       return;
     }
     this.progreso += valor;
-
     this.cambioValor.emit(this.progreso);
   }
 }
