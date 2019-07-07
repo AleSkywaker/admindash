c84d95a91a1985f6671fd0e75059175672474666
Alex Colombo <uricaine@hotmail.com>
2019-07-07 09:33:26 +0200

    fix tick

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -7,13 +7,22 @@ import { DOCUMENT } from '@angular/common';
   styleUrls: ['./account-settings.component.css']
 })
 export class AccountSettingsComponent implements OnInit {
-  constructor(@Inject(DOCUMENT) private document: any) {}
+  constructor(@Inject(DOCUMENT) private documento: any) {}
 
   ngOnInit() {}
 
-  cambiarColor(tema: string) {
-    console.log(tema);
+  cambiarColor(tema: string, link: any) {
+    console.log(link);
+    this.aplicarCheck(link);
     const url: string = `assets/css/colors/${tema}.css`;
-    this.document.getElementById('tema').setAttribute('href', url);
+    this.documento.getElementById('tema').setAttribute('href', url);
+  }
+
+  aplicarCheck(link: any) {
+    const selectores: any = document.getElementsByClassName('selector');
+    for (const ref of selectores) {
+      ref.classList.remove('working');
+    }
+    link.classList.add('working');
   }
 }
