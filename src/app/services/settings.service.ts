e0563ac9f60f6bcf16eb0901d48cb0577cba88a0
Alex Colombo <uricaine@hotmail.com>
2019-07-09 19:20:17 +0200

    save in localStorage created

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -12,6 +12,15 @@ export class SettingsService {
 
   guardarAjustes() {
     localStorage.setItem('ajustes', JSON.stringify(this.ajustes));
+    console.log('Guardado en localStorage');
+  }
+  cargarAjustes() {
+    if (localStorage.getItem('ajustes')) {
+      this.ajustes = JSON.parse(localStorage.getItem('ajustes'));
+      console.log('Cargando del localStorage');
+    } else {
+      console.log('Usando valores por defecto');
+    }
   }
 }
 
