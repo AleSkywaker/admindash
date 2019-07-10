f2c7dd00808047dfbfc9f3f16b2aa212c13d2951
Alex Colombo <uricaine@hotmail.com>
2019-07-10 19:15:23 +0200

    inject ajustesService

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
-@Injectable()
-// {providedIn: 'root'}
+@Injectable({ providedIn: 'root' })
+//
 export class SettingsService {
   ajustes: Ajustes = {
     temaUrl: 'assets/css/colors/default.css',
     tema: 'default'
   };
 
-  constructor() {}
+  // tslint:disable-next-line: deprecation
+  constructor(@Inject(DOCUMENT) private documento: any) {
+    this.cargarAjustes();
+  }
 
   guardarAjustes() {
     localStorage.setItem('ajustes', JSON.stringify(this.ajustes));
@@ -17,10 +21,20 @@ export class SettingsService {
   cargarAjustes() {
     if (localStorage.getItem('ajustes')) {
       this.ajustes = JSON.parse(localStorage.getItem('ajustes'));
+      this.aplicarTema(this.ajustes.tema);
       console.log('Cargando del localStorage');
     } else {
       console.log('Usando valores por defecto');
     }
+  }
+  aplicarTema(tema: string) {
+    const url: string = `assets/css/colors/${tema}.css`;
+    this.documento.getElementById('tema').setAttribute('href', url);
+
+    this.ajustes.temaUrl = url;
+    this.ajustes.tema = tema;
+
+    this.guardarAjustes();
   }
 }
 
