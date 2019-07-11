d205665bc4f32c96d00c65639c0aeace372c648a
Alex Colombo <uricaine@hotmail.com>
2019-07-11 23:23:04 +0200

    init method colocarCheck

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -9,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class AccountSettingsComponent implements OnInit {
   constructor(public ajustesService: SettingsService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.colocarCheck();
+  }
 
   cambiarColor(tema: string, link: any) {
     console.log('link', link);
