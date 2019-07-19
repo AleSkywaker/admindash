e8e5dfe8865efd1ea95ae4730f973b0a65720a6b
Alex Colombo <uricaine@hotmail.com>
2019-07-19 15:41:35 +0200

    change import of settingSservices

--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsService } from './service.index';
+import { SharedService } from './service.index';
+import { SidebarService } from './service.index';
 
 @NgModule({
   declarations: [],
