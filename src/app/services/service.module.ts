7aaf2c8efe83cc26dd374e63cb006174453cc295
Alex Colombo <uricaine@hotmail.com>
2019-07-21 20:00:06 +0200

    create service module

--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SettingsService } from './service.index';
-import { SharedService } from './service.index';
-import { SidebarService } from './service.index';
+import { SettingsService, SharedService, SidebarService } from './service.index';
 
 @NgModule({
   declarations: [],
