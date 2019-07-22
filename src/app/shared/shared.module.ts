63c736242acff035b30498efeaaa0f278e550da0
Alex Colombo <uricaine@hotmail.com>
2019-07-22 21:25:07 +0200

    import modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { HeaderComponent } from './header/header.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 @NgModule({
+  imports: [RouterModule, CommonModule],
   declarations: [NopagefoundComponent, HeaderComponent, SidebarComponent, BreadcrumbsComponent],
   exports: [NopagefoundComponent, HeaderComponent, SidebarComponent, BreadcrumbsComponent]
 })
