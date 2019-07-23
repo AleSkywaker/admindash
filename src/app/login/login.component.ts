524564b2480b8756ceb0d89f490177eb81724ceb
Alex Colombo <uricaine@hotmail.com>
2019-07-23 20:12:32 +0200

    call init plugins from login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+declare function init_plugins(): any;
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,7 +11,9 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   constructor(public router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    init_plugins();
+  }
 
   ingresar() {
     this.router.navigate(['/dashboard']);
