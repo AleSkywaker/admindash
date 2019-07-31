319b757876213e4420c448b9b4ec9b07899ac49f
Alex Colombo <uricaine@hotmail.com>
2019-07-31 21:44:47 +0200

    inyect meta from platform-browser

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
-import { Title } from '@angular/platform-browser';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -10,7 +10,7 @@ import { Title } from '@angular/platform-browser';
 })
 export class BreadcrumbsComponent implements OnInit {
   public titulo: string;
-  constructor(private router: Router, private title: Title) {
+  constructor(private router: Router, private title: Title, private meta: Meta) {
     this.getDataRoute().subscribe(data => {
       this.titulo = data.titulo;
       this.title.setTitle(data.titulo);
