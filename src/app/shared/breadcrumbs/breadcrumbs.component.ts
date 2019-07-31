874050a04a288487ea156f37d8961c72028a0167
Alex Colombo <uricaine@hotmail.com>
2019-07-31 21:41:06 +0200

    set tittle

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -8,9 +9,12 @@ import { filter, map } from 'rxjs/operators';
   styles: []
 })
 export class BreadcrumbsComponent implements OnInit {
-  constructor(private router: Router) {
+  public titulo: string;
+  constructor(private router: Router, private title: Title) {
     this.getDataRoute().subscribe(data => {
       console.log(data);
+      this.titulo = data.titulo;
+      this.title.setTitle(data.titulo);
     });
   }
 
